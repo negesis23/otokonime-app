425d44141e4f3cbf5a457295a04686d8ffef336b
Negesis23 <rangganopian50@gmail.com>
2023-10-06 13:39:36 +0700

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import routes from './routes';
+import {
+ defineComponent,
+ onMounted
+} from 'vue';
+import {
+ f7,
+ f7ready
+} from 'framework7-vue';
+import capacitorApp from './utils/capacitor-app.js';
 
-const f7Params = {
- name: 'Otokonime',
- theme: 'md',
- darkMode: true,
- colors: {
-  primary: '#6A3DE8'
- },
- routes,
- statusbar: {
-  iosOverlaysWebView: true,
-  androidOverlaysWebView: false,
-  androidBackgroundColor: '#000000',
- }
-}
 
-export default () => (
- <f7-app {...f7Params}>
+export default defineComponent({
+ setup() {
+
+  const f7Params = {
+   name: 'Otokonime',
+   theme: 'md',
+   darkMode: true,
+   colors: {
+    primary: '#6A3DE8'
+   },
+   routes,
+   statusbar: {
+    enabled: true,
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: false,
+    androidBackgroundColor: '#161220',
+    androidTextColor: 'white',
+   }
+  }
+
+  onMounted(() => {
+   f7ready(() => {
+    capacitorApp.init(f7);
+   });
+  });
+
+  return () => (
+   <f7-app {...f7Params}>
   <f7-view main class="safe-areas" url="/"></f7-view>
  </f7-app>
-);
+  )
+ }
+});
